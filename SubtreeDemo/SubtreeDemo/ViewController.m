05f584dae01b9e966a257809d2011fe5e5b77d87
Wilson Yuan <yuansc@nihao.com>
2017-03-09 16:53:38 +0800

    update

--- SubtreeDemo/SubtreeDemo/ViewController.m
+++ SubtreeDemo/SubtreeDemo/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "StaticViewController.h"
+#import "StaticLibrary.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [StaticLibrary hello];
+    
+    
+    StaticViewController *vc = [[StaticViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 
